6bef830dbabab3081c04de7320b5cd3603439b1e
ahung13 <denisantonov58863@gmail.com>
2023-01-18 23:48:31 +0400

    Added attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,12 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import page.PageElements;
 
 
@@ -15,6 +17,11 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1080x800";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
 
     }
 
@@ -26,6 +33,9 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
